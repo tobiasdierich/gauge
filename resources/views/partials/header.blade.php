6fd6128f9347619fe67b49d28f2538c580e89355
Tobias Dierich <git@tobiasdierich.de>
2020-10-08 15:31:52 +0200

    Update header logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full flex space-x-4">
     <div class="w-64 px-4">
         <div class="py-4">
-            <a class="font-bold text-2xl tracking-wide hover:text-indigo-600" href="#">
+            <a class="px-2 font-bold text-3xl tracking-wide hover:text-indigo-600" href="{{ route('gauge.dashboard') }}">
                 Gauge
             </a>
         </div>
